683dcdf58fe594b98f1f6aa5d0549a9775795437
Zach Leslie <zach@puppetlabs.com>
2011-07-08 12:30:11 -0700

    initial unbound module

---
+++ manifests/init.pp
@@ -0,0 +1,36 @@
+class unbound {
+  include unbound::params
+
+  $unbound_confdir = $unbound::unbound_confdir
+  $unbound_logdir  = $unbound::unbound_logdir
+  $unbound_service = $unbound::service
+  $unbound_conf    = "$unbound_confdir/unbound.conf"
+
+  package { "unbound"]:
+    ensure   => installed,
+    provider => $kernel ? {
+      Darwin  => macports,
+      default => undef,
+    }
+  }
+
+  service { "unbound":
+    name   => $unbound_service
+    ensure => running,
+    enable => true,
+  }
+
+  concat::fragment { 'unbound-header':
+    order   => '00',
+    target  => "$unbound_conf",
+    content => template("unbound/unbound.conf.erb"),
+  }
+
+  Concat::Fragment <| target == "$unbound_conf" |>
+
+  concat { "$unbound_conf":
+    notify => Service["unbound"],
+  }
+
+}
+
---
+++ manifests/params.pp
@@ -0,0 +1,14 @@
+class unbound::params {
+ case $operatingsystem {
+  'ubuntu', 'debian': {
+    $unbound_confdir = '/etc/unbound'
+    $unbound_logdir = '/var/log/unbound'
+    $unbound_service = 'unbound'
+  }
+  'darwin': {
+    $unbound_confdir = '/opt/local/etc/unbound'
+    $unbound_logdir = '/opt/local/var/log/unbound'
+    $unbound_service = 'org.macports.unbound'
+  }
+ }
+}
