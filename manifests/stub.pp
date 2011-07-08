49ff92c23ab0ef480d5e63d8a99de915bf177445
Zach Leslie <zach@puppetlabs.com>
2011-07-08 14:41:43 -0700

    include needed params

--- manifests/stub.pp
+++ manifests/stub.pp
@@ -1,4 +1,7 @@
 define unbound::stub ($address) {
+  include unbound::params
+
+  $unbound_confdir = $unbound::params::unbound_confdir
 
   concat::fragment { 'unbound-stube-$name':
     order   => '05',
