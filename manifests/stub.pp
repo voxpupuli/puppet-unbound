0c35e84cf56e1cd1cec0df1361aa98c469ed6859
Zach Leslie <zach@puppetlabs.com>
2011-08-30 15:17:53 -0700

    add inseucre option to unbound stub, so that we can resolve internal names without dnssec

--- manifests/stub.pp
+++ manifests/stub.pp
@@ -1,12 +1,23 @@
-define unbound::stub ($address) {
+define unbound::stub (
+  $address,
+  $insecure = false
+) {
   include unbound::params
 
   $unbound_confdir = $unbound::params::unbound_confdir
 
-  concat::fragment { 'unbound-stub-$name':
+  concat::fragment { "unbound-stub-$name":
     order   => '05',
     target  => "$unbound_confdir/unbound.conf",
     content => template("unbound/stub.erb"),
+  }
+
+  if $insecure == true {
+    concat::fragment { :
+      order   => '01',
+      target  => "$unbound_confdir/unbound.conf",
+      content => "  domain-insecure: \"${name}\"",
+    }
   }
 
 }
