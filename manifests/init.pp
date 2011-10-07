6db071747640c530b88a7f05d9c959d9cb730f85
Zach Leslie <zach@puppetlabs.com>
2011-10-07 14:28:23 -0700

    add unbound caching dns on mawu add interface and access variables and make them work in the template make interface and access arrays and strings work properly in template for unbound

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,9 +1,13 @@
-class unbound {
+class unbound (
+  $verbosity = 1,
+  $interface = ["::0","0.0.0.0"],
+  $access    = ["::1","127.0.0.1/8"]
+  ) {
   include unbound::params
 
   $unbound_confdir = $unbound::params::unbound_confdir
   $unbound_logdir  = $unbound::params::unbound_logdir
-  $unbound_service = $unbound::params::service
+  $unbound_service = $unbound::params::unbound_service
 
   package { "unbound":
     ensure   => installed,
@@ -17,7 +21,7 @@ class unbound {
     name      => $unbound_service,
     ensure    => running,
     enable    => true,
-    pattern   => "unbound",
+    #pattern   => "unbound",
     hasstatus => false,
   }
 
@@ -28,10 +32,10 @@ class unbound {
   }
 
   concat { "$unbound_confdir/unbound.conf":
-    notify => Service["unbound"],
+    notify => Service[$unbound_service],
   }
 
-  file { "$unbount_confdir/root.key":
+  file { "$unbound_confdir/root.key":
     owner   => "unbound",
     group   => "unbound",
     content => ". IN DS 19036 8 2 49AAC11D7B6F6446702E54A1607371607A1A41855200FD2CE1CDDE32F24E8FB5",
