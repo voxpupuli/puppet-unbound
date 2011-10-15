ee91aa87f29ee103528beeccd6ca7767577c3957
Zach Leslie <zach@puppetlabs.com>
2011-10-14 17:17:07 -0700

    test variable escaping for strange concat issue

--- manifests/stub.pp
+++ manifests/stub.pp
@@ -13,14 +13,14 @@ define unbound::stub (
   }
 
   if $insecure == true {
-    concat::fragment { "unbound-stub-$name-insecure":
+    concat::fragment { "unbound-stub-${name}-insecure":
       order   => '01',
       target  => "$unbound_confdir/unbound.conf",
       content => "  domain-insecure: \"${name}\"\n",
     }
   }
 
-  concat::fragment { "unbound-stub-$name-local-zone":
+  concat::fragment { "unbound-stub-${name}-local-zone":
     order   => '02',
     target  => "$unbound_confdir/unbound.conf",
     content => "  local-zone: \"${name}\" transparent \n",
