3025eb86d4a932debc2408374d3bcc886cbf495e
Zach Leslie <zach@puppetlabs.com>
2011-11-20 13:58:47 -0800

    add tests

---
+++ tests/init.pp
@@ -0,0 +1,16 @@
+class {
+  "unbound":
+    interface => ["::0","0.0.0.0"],
+    access    => ["fe80::/10","10.0.0.0/24"],
+}
+
+unbound::stub { "example.com":
+  address  => '10.0.0.10',
+  insecure => true,
+}
+
+unbound::stub { "0.0.0.10.in-addr.arpa.":
+  address  => '10.0.0.10',
+  insecure => true,
+}
+
