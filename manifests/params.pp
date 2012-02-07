aff5f510ff03490954067ffa604246ca45edfad7
Zach Leslie <zach@puppetlabs.com>
2012-02-07 01:43:22 -0800

    add package differences to params class

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,16 +1,19 @@
 class unbound::params {
  case $operatingsystem {
   'ubuntu', 'debian': {
+    $unboudn_package = 'unbound'
     $unbound_confdir = '/etc/unbound'
-    $unbound_logdir = '/var/log'
+    $unbound_logdir  = '/var/log'
     $unbound_service = 'unbound'
   }
   'darwin': {
+    $unboudn_package = 'unbound'
     $unbound_confdir = '/opt/local/etc/unbound'
-    $unbound_logdir = '/opt/local/var/log/unbound'
+    $unbound_logdir  = '/opt/local/var/log/unbound'
     $unbound_service = 'org.macports.unbound'
   }
   'freebsd': {
+    $unboudn_package = 'dns/unbound'
     $unbound_confdir = '/usr/local/etc/unbound'
     $unbound_logdir  = '/usr/local/var/log/unbound'
     $unbound_service = 'unbound'
