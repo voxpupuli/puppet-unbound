e0bb40aeb7c2df8501090491ef0d2f2c740a2796
Steffen Zieger <me@saz.sh>
2012-06-26 14:21:38 +0200

    require unbound class, style-guide changes

--- manifests/stub.pp
+++ manifests/stub.pp
@@ -3,27 +3,27 @@ define unbound::stub (
   $insecure = false
 ) {
   include unbound::params
+  require unbound
 
-  $unbound_confdir = $unbound::params::unbound_confdir
+  $unbound_confdir = "${unbound::params::unbound_confdir}/"
 
-  concat::fragment { "unbound-stub-$name":
-    order   => '05',
-    target  => "$unbound_confdir/unbound.conf",
-    content => template("unbound/stub.erb"),
+  concat::fragment { "unbound-stub-${name}":
+    order   => 05,
+    target  => "${unbound_confdir}/unbound.conf",
+    content => template('unbound/stub.erb'),
   }
 
   if $insecure == true {
     concat::fragment { "unbound-stub-${name}-insecure":
-      order   => '01',
-      target  => "$unbound_confdir/unbound.conf",
+      order   => 01,
+      target  => "${unbound_confdir}/unbound.conf",
       content => "  domain-insecure: \"${name}\"\n",
     }
   }
 
   concat::fragment { "unbound-stub-${name}-local-zone":
-    order   => '02',
-    target  => "$unbound_confdir/unbound.conf",
-    content => "  local-zone: \"${name}\" transparent \n",
+    order   => 02,
+    target  => "${unbound_confdir}/unbound.conf",
+    content => "  local-zone: \"${name}\" transparent\n",
   }
-
 }
