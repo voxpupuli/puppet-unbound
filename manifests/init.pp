3d3077a3ca58d11e2b29ce9e15acc996f8f44a07
Steffen Zieger <me@saz.sh>
2012-06-26 14:21:23 +0200

    add additional parameters, style-guide changes
    Conflicts:
    	manifests/init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,9 +1,14 @@
 class unbound (
   $verbosity = 1,
-  $interface = ["::0","0.0.0.0"],
-  $access    = ["::1","127.0.0.1/8"]
+  $interface = ['::0','0.0.0.0'],
+  $access    = ['::1','127.0.0.1/8'],
+  $outgoing_interface = undef,
+  $statistics_interval = 0,
+  $statistics_cumulative = false,
+  $num_threads = 1,
+  $private_domain = undef,
+  $prefetch = false
 ) {
-
   include unbound::params
   include concat::setup
 
@@ -13,13 +18,19 @@ class unbound (
   $unbound_logdir  = $unbound::params::unbound_logdir
   $unbound_service = $unbound::params::unbound_service
 
+  $provider = $::kernel ? {
+    Darwin  => 'macports',
+    default => undef,
+  }
+
   package { $unbound_package:
     ensure   => installed,
+    provider => $provider,
   }
 
   service { $unbound_service:
-    name      => $unbound_service,
     ensure    => running,
+    name      => $unbound_service,
     enable    => true,
     hasstatus => false,
     require   => Package[$unbound_package],
@@ -28,7 +39,7 @@ class unbound (
   concat::fragment { 'unbound-header':
     order   => '00',
     target  => "${unbound_confdir}/unbound.conf",
-    content => template("unbound/unbound.conf.erb"),
+    content => template('unbound/unbound.conf.erb'),
     require => Package[$unbound_package],
   }
 
@@ -37,11 +48,10 @@ class unbound (
     require => Package[$unbound_package],
   }
 
-  # Initialize the root key file if it doesn't already exist.
   file { "${unbound_confdir}/root.key":
-    owner   => "unbound",
+    owner   => 'unbound',
     group   => 0,
-    content => ". IN DS 19036 8 2 49AAC11D7B6F6446702E54A1607371607A1A41855200FD2CE1CDDE32F24E8FB5",
+    content => '. IN DS 19036 8 2 49AAC11D7B6F6446702E54A1607371607A1A41855200FD2CE1CDDE32F24E8FB5',
     replace => false,
     require => Package[$unbound_package],
   }
