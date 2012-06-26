b88ff7f22265430265d4abe9683e11380ba91e7b
Steffen Zieger <me@saz.sh>
2012-06-26 14:46:57 +0200

    add unbound remote config class

---
+++ manifests/remote.pp
@@ -0,0 +1,18 @@
+class unbound::remote (
+  $interface = ['::1', '127.0.0.1'],
+  $port = 953,
+  $server_key_file = undef,
+  $server_cert_file = undef,
+  $control_key_file = undef,
+  $control_cert_file = undef
+) {
+  include unbound::params
+
+  $unbound_confdir = "${unbound::params::unbound_confdir}/"
+
+  concat::fragment { 'unbound-remote':
+    order   => 10,
+    target  => "${unbound_confdir}unbound.conf",
+    content => template('unbound/remote.erb'),
+  }
+}
