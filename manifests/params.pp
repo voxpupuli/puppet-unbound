3cbe62692ad3c82919a996990333fee47f1354b0
Zach Leslie <zach@puppetlabs.com>
2013-12-12 20:41:21 -0800

    (maint) Change parameter names and cleanup
    This work udpates the parameter names for less typing and overall
    cleaner code.  There are some README udpates to give a bit more
    information as well as some fixes of some unused code.
    
    Note, you may have to update your parameter names, though most of the
    param names were internal and most likely would not need overwriting.

--- manifests/params.pp
+++ manifests/params.pp
@@ -6,46 +6,48 @@ class unbound::params {
 
   case $::operatingsystem {
     'ubuntu', 'debian': {
-      $unbound_confdir     = '/etc/unbound'
-      $unbound_logdir      = '/var/log'
-      $unbound_service     = 'unbound'
-      $unbound_package     = 'unbound'
-      $unbound_anchor_file = 'root.key'
-      $owner               = 'unbound'
+      $confdir      = '/etc/unbound'
+      $logdir       = '/var/log'
+      $service_name = 'unbound'
+      $package_name = 'unbound'
+      $anchor_file  = 'root.key'
+      $owner        = 'unbound'
     }
     'redhat', 'centos', 'scientific': {
-      $unbound_confdir     = '/etc/unbound'
-      $unbound_logdir      = '/var/log'
-      $unbound_service     = 'unbound'
-      $unbound_package     = 'unbound'
-      $unbound_anchor_file = 'root.anchor'
-      $owner               = 'unbound'
+      $confdir      = '/etc/unbound'
+      $logdir       = '/var/log'
+      $service_name = 'unbound'
+      $package_name = 'unbound'
+      $anchor_file  = 'root.anchor'
+      $owner        = 'unbound'
       }
     'darwin': {
-      $unbound_confdir     = '/opt/local/etc/unbound'
-      $unbound_logdir      = '/opt/local/var/log/unbound'
-      $unbound_service     = 'org.macports.unbound'
-      $unbound_package     = 'unbound'
-      $unbound_anchor_file = 'root.key'
-      $owner               = 'unbound'
+      $confdir          = '/opt/local/etc/unbound'
+      $logdir           = '/opt/local/var/log/unbound'
+      $service_name     = 'org.macports.unbound'
+      $package_name     = 'unbound'
+      $package_provider = 'macports'
+      $anchor_file      = 'root.key'
+      $owner            = 'unbound'
     }
     'freebsd': {
-      $unbound_confdir     = '/usr/local/etc/unbound'
-      $unbound_logdir      = '/var/log/unbound'
-      $unbound_service     = 'unbound'
-      $unbound_package     = 'dns/unbound'
-      $unbound_anchor_file = 'root.key'
-      $owner               = 'unbound'
+      $confdir      = '/usr/local/etc/unbound'
+      $logdir       = '/var/log/unbound'
+      $service_name = 'unbound'
+      $package_name = 'dns/unbound'
+      $anchor_file  = 'root.key'
+      $owner        = 'unbound'
     }
     'openbsd': {
-      $unbound_confdir     = '/var/unbound/etc'
-      $unbound_logdir      = '/var/log/unbound'
-      $unbound_service     = 'unbound'
-      $unbound_package     = 'unbound'
-      $unbound_anchor_file = 'root.key'
-      $owner               = '_unbound'
+      $confdir      = '/var/unbound/etc'
+      $logdir       = '/var/log/unbound'
+      $service_name = 'unbound'
+      $package_name = 'unbound'
+      $anchor_file  = 'root.key'
+      $owner        = '_unbound'
     }
   }
 
-  $unbound_hints_file = 'root.hints'
+  $config_file = "${confdir}/unbound.conf"
+  $hints_file  = 'root.hints'
 }
--- manifests/stub.pp
+++ manifests/stub.pp
@@ -1,32 +1,33 @@
 # Class: unbound::stub
 #
-# Create an unbound stub zone for caching upstream name resolvers.
+# Create an unbound stub zone for caching upstream name resolvers
 #
 define unbound::stub (
   $address,
   $insecure = false
 ) {
+
   include unbound::params
 
-  $unbound_confdir = $unbound::params::unbound_confdir
+  $config_file = $unbound::params::config_file
 
   concat::fragment { "unbound-stub-${name}":
     order   => '05',
-    target  => "${unbound_confdir}/unbound.conf",
+    target  => $config_file,
     content => template('unbound/stub.erb'),
   }
 
   if $insecure == true {
     concat::fragment { "unbound-stub-${name}-insecure":
       order   => '01',
-      target  => "${unbound_confdir}/unbound.conf",
+      target  => $config_file,
       content => "  domain-insecure: \"${name}\"\n",
     }
   }
 
   concat::fragment { "unbound-stub-${name}-local-zone":
     order   => '02',
-    target  => "${unbound_confdir}/unbound.conf",
+    target  => $config_file,
     content => "  local-zone: \"${name}\" transparent \n",
   }
 }
