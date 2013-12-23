af026245618c2d3f3f2e3a92e62bcb8e0601fd10
Igor Galić <i.galic@brainsware.org>
2013-12-23 12:55:59 +0100

    enable simple creation of reverse entries

--- manifests/record.pp
+++ manifests/record.pp
@@ -4,16 +4,25 @@
 #
 define unbound::record (
   $content,
-  $ttl  = '14400',
-  $type = 'A'
+  $ttl     = '14400',
+  $type    = 'A',
+  $reverse = false
 ) {
   include unbound::params
 
   $unbound_confdir = $unbound::params::unbound_confdir
 
+  $local_data     = "  local-data: \"${name} ${ttl} IN ${type} ${content}\"\n"
+  $local_data_ptr = "  local-data-ptr: \"${content} ${name}\"\n"
+
+  $entry = $reverse? {
+    true    => "${local_data}${local_data_ptr}",
+    default => $local_data,
+  }
+
   concat::fragment { "unbound-stub-${name}-local-record":
     order   => '02',
     target  => "${unbound_confdir}/unbound.conf",
-    content => "  local-data: \"${name} ${ttl} IN ${type} ${content}\"\n",
+    content => $entry,
   }
 }
