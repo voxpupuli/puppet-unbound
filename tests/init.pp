a24c2caf53221fd1d6c62123aeaa2efde9906de4
Zach Leslie <zach@puppetlabs.com>
2014-08-26 19:51:26 -0700

    Update test files to pass lint

--- tests/init.pp
+++ tests/init.pp
@@ -1,23 +1,22 @@
-class {
-  "unbound":
-    interface => ["::0","0.0.0.0"],
-    access    => ["fe80::/10","10.0.0.0/24"],
+class { 'unbound':
+  interface => ['::0','0.0.0.0'],
+  access    => ['fe80::/10','10.0.0.0/24'],
 }
 
-unbound::stub { "example.com":
+unbound::stub { 'example.com':
   address  => '10.0.0.10',
   insecure => true,
 }
 
-unbound::stub { "0.0.0.10.in-addr.arpa.":
+unbound::stub { '0.0.0.10.in-addr.arpa.':
   address  => '10.0.0.10',
   insecure => true,
 }
 
-unbound::local_zone { "10.in-addr.arpa.":
-  type => "nodefault"
+unbound::local_zone { '10.in-addr.arpa.':
+  type => 'nodefault'
 }
 
-unbound::forward { "10.in-addr.arpa.":
+unbound::forward { '10.in-addr.arpa.':
   address  => '10.0.0.10',
 }
