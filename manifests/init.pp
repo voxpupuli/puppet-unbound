fb03f913024f80535d2a01f40dd6ffb5de766158
Zach Leslie <xaque208@gmail.com>
2014-09-29 17:18:57 -0700

    Revert "rebase of pull 44"

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,66 +3,48 @@
 # Installs and configures Unbound, the caching DNS resolver from NLnet Labs
 #
 class unbound (
-  $access                       = $unbound::params::access,
-  $anchor_file                  = $unbound::params::anchor_file,
-  $chroot                       = $unbound::params::chroot,
-  $conf_d                       = $unbound::params::conf_d,
-  $confdir                      = $unbound::params::confdir,
-  $config_file                  = $unbound::params::config_file,
-  $control_enable               = $unbound::params::control_enable,
-  $directory                    = $unbound::params::directory,
-  $dlv_anchor_file              = $unbound::params::dlv_anchor_file,
-  $do_ip4                       = $unbound::params::do_ip4,
-  $do_ip6                       = $unbound::params::do_ip6,
-  $edns_buffer_size             = $unbound::params::edns_buffer_size,
-  $extended_statistics          = $unbound::params::extended_statistics,
-  $fetch_client                 = $unbound::params::fetch_client,
-  $group                        = $unbound::params::group,
-  $harden_below_nxdomain        = $unbound::params::harden_below_nxdomain,
-  $harden_dnssec_stripped       = $unbound::params::harden_dnssec_stripped,
-  $harden_glue                  = $unbound::params::harden_glue,
-  $harden_referral_path         = $unbound::params::harden_referral_path,
-  $hints_file                   = $unbound::params::hints_file,
-  $infra_cache_slabs            = $unbound::params::infra_cache_slabs,
-  $infra_host_ttl               = $unbound::params::infra_host_ttl,
-  $interface                    = $unbound::params::interface,
-  $interface_automatic          = $unbound::params::interface_automatic,
-  $key_cache_size               = $unbound::params::key_cache_size,
-  $key_cache_slabs              = $unbound::params::key_cache_slabs,
-  $keys_d                       = $unbound::params::keys_d,
-  $log_time_ascii               = $unbound::params::log_time_ascii,
-  $logdir                       = $unbound::params::logdir,
-  $module_config                = $unbound::params::module_config,
-  $msg_cache_size               = $unbound::params::msg_cache_size,
-  $msg_cache_slabs              = $unbound::params::msg_cache_slabs,
-  $num_queries_per_thread       = $unbound::params::num_queries_per_thread,
-  $num_threads                  = $unbound::params::num_threads,
-  $outgoing_interface           = $unbound::params::outgoing_interface,
-  $outgoing_port_avoid          = $unbound::params::outgoing_port_avoid,
-  $outgoing_port_permit         = $unbound::params::outgoing_port_permit,
-  $outgoing_range               = $unbound::params::outgoing_range,
-  $owner                        = $unbound::params::owner,
-  $package_name                 = $unbound::params::package_name,
-  $package_provider             = $unbound::params::package_provider,
-  $port                         = $unbound::params::port,
-  $prefetch                     = $unbound::params::prefetch,
-  $prefetch_key                 = $unbound::params::prefetch_key,
-  $private_domain               = $unbound::params::private_domain,
-  $root_hints_url               = $unbound::params::root_hints_url,
-  $rrset_cache_size             = $unbound::params::rrset_cache_size,
-  $rrset_cache_slabs            = $unbound::params::rrset_cache_slabs,
-  $service_name                 = $unbound::params::service_name,
-  $so_rcvbuf                    = $unbound::params::so_rcvbuf,
-  $statistics_cumulative        = $unbound::params::statistics_cumulative,
-  $statistics_interval          = $unbound::params::statistics_interval,
-  $tcp_upstream                 = $unbound::params::tcp_upstream,
-  $trusted_keys_file            = $unbound::params::trusted_keys_file,
-  $unwanted_reply_threshold     = $unbound::params::unwanted_reply_threshold,
-  $use_caps_for_id              = $unbound::params::use_caps_for_id,
-  $val_clean_additional         = $unbound::params::val_clean_additional,
-  $val_log_level                = $unbound::params::val_log_level,
-  $val_permissive_mode          = $unbound::params::val_permissive_mode,
-  $verbosity                    = $unbound::params::verbosity,
+  $verbosity              = 1,
+  $interface              = ['::0','0.0.0.0'],
+  $access                 = ['::1','127.0.0.1/8'],
+  $do_ip4                 = true,
+  $do_ip6                 = true,
+  $interface_automatic    = false,
+  $outgoing_interface     = undef,
+  $extended_statistics    = no,
+  $statistics_interval    = 0,
+  $statistics_cumulative  = false,
+  $control_enable         = false,
+  $num_threads            = 1,
+  $private_domain         = undef,
+  $prefetch               = false,
+  $prefetch_key           = false,
+  $infra_host_ttl         = undef,
+  $edns_buffer_size       = undef,
+  $port                   = 53,
+  $confdir                = $unbound::params::confdir,
+  $config_file            = $unbound::params::config_file,
+  $logdir                 = $unbound::params::logdir,
+  $service_name           = $unbound::params::service_name,
+  $package_name           = $unbound::params::package_name,
+  $package_provider       = $unbound::params::package_provider,
+  $anchor_file            = $unbound::params::anchor_file,
+  $hints_file             = $unbound::params::hints_file,
+  $owner                  = $unbound::params::owner,
+  $fetch_client           = $unbound::params::fetch_client,
+  $root_hints_url         = 'http://www.internic.net/domain/named.root',
+  $msg_cache_slabs        = undef,
+  $rrset_cache_slabs      = undef,
+  $infra_cache_slabs      = undef,
+  $key_cache_slabs        = undef,
+  $rrset_cache_size       = undef,
+  $msg_cache_size         = undef,
+  $key_cache_size         = undef,
+  $num_queries_per_thread = undef,
+  $outgoing_range         = undef,
+  $so_rcvbuf              = undef,
+  $tcp_upstream           = false,
+  $hide_identity          = true,
+  $hide_version           = true,
 ) inherits unbound::params {
 
   if $package_name {
@@ -70,9 +52,9 @@ class unbound (
       ensure   => installed,
       provider => $package_provider,
     }
-    Package[$package_name] -> Service[$service_name]
-    Package[$package_name] -> Concat[$config_file]
-    Package[$package_name] -> File[$anchor_file]
+    Package["$package_name"] -> Service["$service_name"]
+    Package["$package_name"] -> Concat["$config_file"]
+    Package["$package_name"] -> File["${confdir}/${anchor_file}"]
   }
 
   service { $service_name:
@@ -83,22 +65,13 @@ class unbound (
   }
 
   exec { 'download-roothints':
-    command => "${fetch_client} ${hints_file} ${root_hints_url}",
-    creates => $hints_file,
+    command => "${fetch_client} ${confdir}/${hints_file} ${root_hints_url}",
+    creates => "${confdir}/${hints_file}",
     path    => ['/usr/bin','/usr/local/bin'],
     before  => [ Concat::Fragment['unbound-header'] ],
   }
 
-  file { [
-    $confdir,
-    $conf_d,
-    $keys_d
-    ]:
-    ensure  => directory,
-    require => Package[$package_name],
-  }
-
-  file { $hints_file:
+  file { "${confdir}/${hints_file}":
     mode => '0444',
   }
 
@@ -113,10 +86,11 @@ class unbound (
   }
 
   # Initialize the root key file if it doesn't already exist.
-  file { $anchor_file:
+  file { "${confdir}/${anchor_file}":
     owner   => $owner,
     group   => 0,
     content => '. IN DS 19036 8 2 49AAC11D7B6F6446702E54A1607371607A1A41855200FD2CE1CDDE32F24E8FB5',
     replace => false,
   }
+
 }
