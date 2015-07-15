6e7d0f363c5d3986e4468f228b4921c2aae940b4
Zach Leslie <zach@puppetlabs.com>
2015-07-15 13:17:10 -0700

    Begin spec testing unbound::stub
    This adds basic validation to avoid regressions in the future for
    supporting singular and multiple addresses passed to the unbound::stub
    define.

---
+++ spec/defines/stub_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+describe 'unbound::stub' do
+
+  let(:facts) {
+    {
+      :operatingsystem => 'OpenBSD',
+      :concat_basedir => '/dne'
+    }
+  }
+
+  context "with a single address" do
+    let(:title) { '10.0.10.in-addr.arpa.' }
+    let(:params) { {:address => 'ns1.example.com'} }
+    it do
+      should contain_unbound__stub__validate_addr('ns1.example.com,10.0.10.in-addr.arpa.')
+    end
+  end
+
+  context "with multiple addresses" do
+    let(:title) { '10.0.10.in-addr.arpa.' }
+    let(:params) { {:address => [ 'ns1.example.com', '10.0.0.10@10053', 'ns2.example.com' ] } }
+
+    it do
+      should contain_unbound__stub__validate_addr('ns1.example.com,10.0.10.in-addr.arpa.')
+      should contain_unbound__stub__validate_addr('10.0.0.1@10053,10.0.10.in-addr.arpa.')
+      should contain_unbound__stub__validate_addr('ns2.example.com,10.0.10.in-addr.arpa.')
+    end
+  end
+
+end
