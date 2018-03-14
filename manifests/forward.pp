e41bf94ad855abb1f75b361216578348615bd181
Zach Leslie <xaque208@gmail.com>
2018-03-13 21:48:12 -0700

    Allow forward-host option

--- manifests/forward.pp
+++ manifests/forward.pp
@@ -4,13 +4,18 @@
 #
 # == Parameters:
 #
-# [*address*]
-#   (required) IP address of server to forward to. Can be IP 4 or IP 6 (and an
-#   array or a single value. To use a nondefault port for DNS communication
-#   append  '@' with the port number.
-#
 # [*zone*]
 #   (required) the name of the zone.
+#
+# [*address*]
+#   IP address of server to forward queries to. Can be IP 4 or IP 6 (and an
+#   array or a single value. To use a nondefault port for DNS communication
+#   append '@' with the port number.
+#
+# [*host*]
+#   Hostname of server to forward queries to. Can be IP 4 or IP 6 (and an array
+#   or a single value. To use a nondefault port for DNS communication append
+#   '@' with the port number.
 #
 # [*forward_first*]
 #   (optional) If enabled, a query is attempted without the forward clause if
@@ -22,10 +27,11 @@
 #   (optional) name of configuration file
 #
 define unbound::forward (
-  $address,
-  $zone          = $name,
+  Array $address                   = [],
+  Array $host                      = [],
+  $zone                            = $name,
   Pattern[/yes|no/] $forward_first = 'no',
-  $config_file   = $unbound::config_file,
+  $config_file                     = $unbound::config_file,
 ) {
 
   concat::fragment { "unbound-forward-${name}":
