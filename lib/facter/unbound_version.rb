ea9f23a5628b4095cec86bf9cd9448853fee2d84
Yannick Denzer <yannick.denzer@stepping-stone.ch>
2018-05-09 10:58:53 +0200

    Fix regular expression in fact `unbound_version`.

--- lib/facter/unbound_version.rb
+++ lib/facter/unbound_version.rb
@@ -9,7 +9,7 @@ unbound_bin = case Facter.value('kernel')
 if File.exist? unbound_bin
   unbound_version = Facter::Util::Resolution.exec(
     "#{unbound_bin} -V 2>&1"
-  ).match(%r{\nVersion\s(\d\.\d\.\d)})[1]
+  ).match(%r{\nVersion\s+(\d+(?:\.\d+)*)})[1]
   Facter.add(:unbound_version) do
     setcode do
       unbound_version
