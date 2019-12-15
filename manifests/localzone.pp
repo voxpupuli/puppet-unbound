b85e062d06baf8e7bc2f5c4e3c61e6bb49509e37
Adrian Eib <adrian.eib@showmax.com>
2019-12-15 21:46:38 +0100

    Add functionality to render local data:   - Add a new param local_data which is an Array of Strings. local_data will be add into a configuration file as defined in a template.   - Add a new param template_name. Therefore anybody can use a custom template.

--- manifests/localzone.pp
+++ manifests/localzone.pp
@@ -9,23 +9,39 @@
 # === Parameters:
 #
 # [*zone*]
-#   (required) String. Zone name
+#   (required) String. Zone name.
 #
 # [*type*]
-#   (required) Custom type Unbound::Local_zone_type
+#   (required) Custom type Unbound::Local_zone_type.
 #
 # [*config_file*]
-#   (optional) name of configuration file
+#   (optional) name of configuration file.
+#
+# [*local_data*]
+#   (optional) Array.
+#   Define local data which should be rendered.
+#   Default value: [].
+#   Example:
+#     unbound::localzone::local_data:
+#       - 'api.test.com 15 IN A 1.1.1.1'
+#       - 'backend.test.com' 15 IN A 1.1.1.1'
+#
+# [*template_name*]
+#   (optional) String.
+#   Use a custom template.
+#   Default value: 'unbound/local_zone.erb'
 #
 define unbound::localzone (
   Unbound::Local_zone_type $type,
   String $zone = $name,
   $config_file = $unbound::config_file,
+  Array[String] $local_data = [],
+  String $template_name = 'unbound/local_zone.erb'
 ) {
 
   concat::fragment { "unbound-localzone-${name}":
     order   => '06',
     target  => $config_file,
-    content => template('unbound/local_zone.erb'),
+    content => template($template_name),
   }
 }
