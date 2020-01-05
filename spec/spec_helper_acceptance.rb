275c433aa0f7226f58ad59776d8e0a0bf4795397
Dennis Hoppe <github@debian-solutions.de>
2020-01-05 23:08:01 +0100

    Clean up spec_helper_acceptance.rb

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,48 +1,31 @@
-# frozen_string_literal: true
-
 require 'beaker-rspec'
 require 'beaker-puppet'
 require 'beaker/puppet_install_helper'
 require 'beaker/module_install_helper'
-require 'beaker-rspec'
 
-modules = [
-  'puppetlabs-stdlib',
-  'puppetlabs-concat'
-]
-def install_modules(host, modules)
-  module_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
-  install_dev_puppet_module_on(host, source: module_root)
-  modules.each do |m|
-    on(host, puppet('module', 'install', m))
-  end
-end
-# Install Puppet on all hosts
-step 'install puppet on all hosts' do
-  run_puppet_install_helper('foss', '6.0.0')
-end
-hosts.each do |host|
-  step "install packages on #{host}"
-  if host['platform'] =~ %r{freebsd}
-    # default installs incorect version
-    host.install_package('dns/bind-tools')
-    host.install_package('puppet4')
-  else
-    # https://github.com/moby/moby/issues/33099
-    if host['platform'] =~ %r{^el-}
-      host.install_package('bind-utils')
-    else
-      host.install_package('dnsutils')
-    end
-    on(host, 'sysctl net.ipv6.conf.all.disable_ipv6=0')
-  end
-  # remove search list and domain from resolve.conf
-  install_modules(host, modules)
-end
+run_puppet_install_helper unless ENV['BEAKER_provision'] == 'no'
+
 RSpec.configure do |c|
+  # Readable test descriptions
   c.formatter = :documentation
-  # c.before :suite do
-  #   hosts.each do |host|
-  #   end
-  # end
+
+  # Configure all nodes in nodeset
+  c.before :suite do
+    install_module
+    install_module_dependencies
+
+    hosts.each do |host|
+      if fact_on(host, 'osfamily') == 'FreeBSD'
+        host.install_package('dns/bind-tools')
+      else
+        if fact_on(host, 'osfamily') == 'RedHat'
+          host.install_package('bind-utils')
+        else
+          host.install_package('dnsutils')
+        end
+
+        on(host, 'sysctl net.ipv6.conf.all.disable_ipv6=0')
+      end
+    end
+  end
 end
