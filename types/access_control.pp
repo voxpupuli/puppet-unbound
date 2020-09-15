c3a820559a0f7252f7f6a08e35e9cf7e6c28cefa
Pat Riehecky <riehecky@fnal.gov>
2020-09-15 08:30:21 -0500

    Resolve puppet-lint notices

--- types/access_control.pp
+++ types/access_control.pp
@@ -1,6 +1,6 @@
 type Unbound::Access_control = Struct[{
-  action    => Optional[Enum['deny', 'refuse', 'allow', 'allow_snoop', 'deny_non_local', 'refuse_non_local']],
-  tags      => Optional[Array[String]],
-  rr_string => Optional[String],
-  view      => Optional[String],
+    action    => Optional[Enum['deny', 'refuse', 'allow', 'allow_snoop', 'deny_non_local', 'refuse_non_local']],
+    tags      => Optional[Array[String]],
+    rr_string => Optional[String],
+    view      => Optional[String],
 }]
--- types/resource_record_type.pp
+++ types/resource_record_type.pp
@@ -1,8 +1,8 @@
 # custom type for resource record used for local-data
 type Unbound::Resource_record_type = Struct[{
-  'name'      => String,
-  'ttl'       => Optional[Integer],
-  'class'     => Optional[String],
-  'type'      => String,
-  'data'      => String,
+    'name'      => String,
+    'ttl'       => Optional[Integer],
+    'class'     => Optional[String],
+    'type'      => String,
+    'data'      => String,
 }]
