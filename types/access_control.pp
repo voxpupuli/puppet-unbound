346dcae0cdd4f47f66d96a081f92f07613a217eb
and0x000 <and0x000@users.noreply.github.com>
2024-06-11 17:47:59 +0200

    Update access_control.pp
    update access control actions

--- types/access_control.pp
+++ types/access_control.pp
@@ -1,5 +1,5 @@
 type Unbound::Access_control = Struct[{
-    action    => Optional[Enum['deny', 'refuse', 'allow', 'allow_snoop', 'deny_non_local', 'refuse_non_local']],
+    action    => Optional[Enum['deny', 'refuse', 'allow', 'allow_setrd', 'allow_snoop', 'allow_cookie', 'deny_non_local', 'refuse_non_local']],
     tags      => Optional[Array[String]],
     rr_string => Optional[String],
     view      => Optional[String],
